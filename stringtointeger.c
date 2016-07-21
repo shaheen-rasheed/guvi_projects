fc663a7e56e284c02f36f71c0b95652b05bf88f6
shaheen-rasheed <shaheenr2012@gmail.com>
2016-07-21 15:15:21 +0530

    Add files via upload

---
+++ stringtointeger.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+int x;
+char a[5];
+clrscr();
+printf("Enter the value:\n");
+scanf("%s",a);
+x=atoi(a);
+printf("the integer value is :%s\n",x);
+getch();
+}
